e546c5d1ca4ce6c08e99e5e584412a5308424932
Mikhail Petrov <mpetrov71@mail.ru>
2026-04-23 15:30:42 +0500

    Correct main.py add 4 exercise match the comments

--- main.py
+++ main.py
@@ -124,7 +124,6 @@ def flat_generator2(list_of_lists):
         else:
             yield list_element
 
-
 def test_4():
 
     list_of_lists_2 = [
@@ -132,6 +131,7 @@ def test_4():
         ['d', 'e', [['f'], 'h'], False],
         [1, 2, None, [[[[['!']]]]], []]
     ]
+
     for flat_iterator_item, check_item in zip(
             flat_generator2(list_of_lists_2),
             ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
